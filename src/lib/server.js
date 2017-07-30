3f824c896209dbe5d5bb76f283bbd0d8bf6a43bb
soulpwrd <martin.pravda@gmail.com>
2017-07-30 18:07:17 +0200

    Add simple hapi server

---
+++ src/lib/server.js
@@ -0,0 +1,49 @@
+const Hapi = require('hapi');
+const Blipp = require('blipp');
+const Inert = require('inert');
+const Hoek = require('hoek');
+
+const { resolve } = require('path');
+
+//routes
+const routes = require('./routes');
+
+// create a new Hapi instance server
+const server = new Hapi.Server({
+  connections: {
+    routes: {
+      files: {
+        relativeTo: resolve(__dirname, '../../dist')
+      }
+    }
+  }
+});
+
+//setup connections
+server.connection({
+  port: process.env.PORT || 8000
+});
+
+// register plugins
+// add routes
+// and start server
+server.register([
+  Inert,
+  Blipp,
+], (err) => {
+
+  if (err) {
+    Hoek.assert(err);
+  }
+
+  server.route(routes);
+
+  server.start((err) => {
+
+    if (err) {
+      Hoek.assert(err);
+    }
+
+  });
+});
+
