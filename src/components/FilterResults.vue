1a9fb61f611c737742cd825dd80fd5268707d935
soulpwrd <martin.pravda@gmail.com>
2017-07-30 14:07:30 +0200

    Add warning

--- src/components/FilterResults.vue
+++ src/components/FilterResults.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="filterResults--wrapper" v-if="results.length > 0">
+  <div class="filterResults--wrapper" v-if="results">
     <div class="filterResults--controls">
-      <button class="btn btn-hide" v-if="visible" v-on:click="setVisible(false)">show</button>
+      <button class="btn btn-hide" v-if="visible" v-on:click="setVisible(false)">hide</button>
       <button class="btn btn-show" v-else v-on:click="setVisible(true)">show</button>
     </div>
     <div class="filterResults" v-show="visible">
       <div class="filterResults--title">
         <h3>AVAILABLE FLIGHTS</h3>
-
       </div>
       <div class="filterResults--main">
-        <ul>
+        <div class="filterResulst--warning" v-show="results.length === 0">
+          <span class="warning"> NO RESULTS FOUND</span>
+        </div>
+        <ul class="filterResults--list" v-show="results.length > 0">
           <li class="filterResults--item" v-bind:key="result.id" v-for="result in results">
             <div class="item--attribute">
               <span>
@@ -44,7 +46,7 @@ export default {
   data() {
     return {
       visible: false,
-      results: []
+      results: undefined
     };
   },
   methods: {
